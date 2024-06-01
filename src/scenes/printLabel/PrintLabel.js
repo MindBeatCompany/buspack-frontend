d2c78b8d87d08b6e708a895a84365efe6818b817
= <=>
2024-05-31 22:16:17 -0300

    Adding the new buttons in FormPieces

--- src/scenes/printLabel/PrintLabel.js
+++ src/scenes/printLabel/PrintLabel.js
@@ -11,9 +11,10 @@ import FormPieces from "./components/FormPieces/FormPieces";
 import BadgeForStatus from "../../components/BadgeForStatus";
 import FileSaver from 'file-saver';
 
+// Function to map data out for print
 function mapDataOut(data, ecoCode) {
   return {
-    idRequest:data[6],
+    idRequest: data[6],
     pieceId: data[0],
     shipping: data[7],
     cpa: data[3],
@@ -21,10 +22,11 @@ function mapDataOut(data, ecoCode) {
     address: data[2],
     status: data[9].props.state,
     ecoCode,
-    city:data[4],
-    province:data[5],
+    city: data[4],
+    province: data[5],
   }
 }
+
 class PrintLabel extends React.Component {
   constructor(props) {
     super(props);
@@ -40,27 +42,30 @@ class PrintLabel extends React.Component {
         this.props.t("printLabelTable.idRequest"),
         this.props.t("printLabelTable.shipping"),
         this.props.t("printLabelTable.tracking"),
-        // this.props.t("printLabelTable.state"),
       ],
       bodyRows: [],
       selectedRowsToPrint: [],
-      showTable: false,      
+      showTable: false,
     };
     this.onChange = this.onChange.bind(this);
     this.onChangeAll = this.onChangeAll.bind(this);
     this.printTickets = this.printTickets.bind(this);
+    this.printTickets2 = this.printTickets2.bind(this);
+    this.printTickets3 = this.printTickets3.bind(this);
+    this.printTickets4 = this.printTickets4.bind(this);
   }
 
-  onChange(e) {    
+  // Handle row selection
+  onChange(e) {
     const value = e.target.value;
-    const checked = e.target.checked;    
-    const { selectedRowsToPrint } = this.state;    
+    const checked = e.target.checked;
+    const { selectedRowsToPrint } = this.state;
     if (checked) {
-      this.setState({        
+      this.setState({
         selectedRowsToPrint: [...selectedRowsToPrint, Number(value)],
       });
     } else {
-      this.setState({ 
+      this.setState({
         selectedRowsToPrint: selectedRowsToPrint.filter(
           (r) => r !== value
         ),
@@ -68,50 +73,26 @@ class PrintLabel extends React.Component {
     }
   }
 
-  onChangeAll(e) {    
+  // Handle select all rows
+  onChangeAll(e) {
     const checked = e.target.checked;
     const { bodyRows } = this.state;
     if (checked) {
-      this.setState({       
-        selectedRowsToPrint : bodyRows.map(          
+      this.setState({
+        selectedRowsToPrint: bodyRows.map(
           (row, idx) => idx
-        ),        
+        ),
       });
-    } else {      
-      this.setState({        
-        selectedRowsToPrint : [],
+    } else {
+      this.setState({
+        selectedRowsToPrint: [],
       });
     }
   }
 
+  // Handle the response from API
   handleResponse(data) {
     const bodyRowsTransformed = [];
-    // data.data = [
-    //   {
-    //       "pieceId": "437803-22",
-    //       "recipientFullname": "Mario Kempes",
-    //       "address": "    ",
-    //       "cpa": "1419",
-    //       "city": "VILLA PUEYRREDON",
-    //       "province": "CABA",
-    //       "requestId": "1000",
-    //       "shipping": "Entrega en domicilio",
-    //       "voucher": "X-2005-71881",
-    //       "status": "En Agencia Origen"
-    //   },
-    //   {
-    //       "pieceId": "437804-22",
-    //       "recipientFullname": "Mario Kempes",
-    //       "address": "    ",
-    //       "cpa": "1419",
-    //       "city": "VILLA PUEYRREDON",
-    //       "province": "CABA",
-    //       "requestId": "1000",
-    //       "shipping": "Entrega en domicilio",
-    //       "voucher": "X-2005-71881",
-    //       "status": "En Agencia Origen"
-    //   }
-    // ];
     const textMock = {
       ["Pieza a retirar en Cliente"]: "PROCESANDO SU PEDIDO",
       ["Pieza en manos del Transportista"]: "EN CAMINO",
@@ -139,11 +120,10 @@ class PrintLabel extends React.Component {
         cpa,
         city,
         province,
-        idRequest:requestId,
+        idRequest: requestId,
         shipping,
         voucher,
         status: <BadgeForStatus state={textMock[row.status]} />,
-        // status: <BadgeForStatus state={tracking.estadodelivery.estado} />,
       };
       bodyRowsTransformed.push([...Object.values(newObj)]);
     })
@@ -151,15 +131,37 @@ class PrintLabel extends React.Component {
     this.setState({ bodyRows: bodyRowsTransformed });
   }
 
+  // Original print function
   printTickets(e) {
-    e.preventDefault();    
-    const { selectedRowsToPrint } = this.state;      
+    e.preventDefault();
+    this.sendToPrint("print-labels/pdf10x15");
+  }
+
+  // Additional print functions
+  printTickets2(e) {
+    e.preventDefault();
+    this.sendToPrint("print-labels/pdf10x20");
+  }
+
+  printTickets3(e) {
+    e.preventDefault();
+    this.sendToPrint("print-labels/pdf10x15-alt");
+  }
+
+  printTickets4(e) {
+    e.preventDefault();
+    this.sendToPrint("print-labels/pdf10x10");
+  }
+
+  // Common function to send data for printing
+  sendToPrint(url) {
+    const { selectedRowsToPrint } = this.state;
     const {
       auth: { userData },
       user,
       sendToPrint,
     } = this.props;
-    
+
     const arrayOfData = selectedRowsToPrint
       .map((num) => this.state.bodyRows[num])
       .map((data) => ({
@@ -167,8 +169,10 @@ class PrintLabel extends React.Component {
         companyName: userData.account.companyName,
         clientName: userData.userName,
         ed: data[7] === "Entrega en domicilio" ? "ED" : "",
+        voucher: data[8],
       }));
     sendToPrint({
+      url,
       data: arrayOfData,
       callback: (blob) => {
         FileSaver.saveAs(blob, 'etiquetas.pdf');
@@ -177,7 +181,7 @@ class PrintLabel extends React.Component {
   }
 
   render() {
-    const { t, history, auth: { userData }, getPrintLabel} = this.props;
+    const { t, history, auth: { userData }, getPrintLabel } = this.props;
     const { theaders, bodyRows } = this.state;
     return (
       <Container fluid className="pl-5 pr-5">
@@ -198,8 +202,11 @@ class PrintLabel extends React.Component {
               setShowTable={() => this.setState({ showTable: true })}
               showTable={this.state.showTable}
               printTickets={this.printTickets}
+              printTickets2={this.printTickets2}
+              printTickets3={this.printTickets3}
+              printTickets4={this.printTickets4}
               getPrintLabel={getPrintLabel}
-              handleResponse={(data)=>this.handleResponse(data)}
+              handleResponse={(data) => this.handleResponse(data)}
             />
           </Col>
           <Col className="mt-3">
--- src/scenes/printLabel/components/FormPieces/FormPieces.js
+++ src/scenes/printLabel/components/FormPieces/FormPieces.js
@@ -4,135 +4,143 @@ import { withTranslation } from "react-i18next";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { Col, Spinner, Form } from "react-bootstrap";
-import * as Yup from "yup";
 import { authActions } from "../../../../state/ducks/auth";
 import { ActionButton, AuthenticatedLink } from "../../../../components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 
 const initialValues = {
-  request_id: "",
-  voucher: "",
-  delivery: "",
+    request_id: "",
+    voucher: "",
+    delivery: "",
 };
 
 const FormPieces = ({
-  t,
-  showTable,
-  setShowTable,
-  printTickets,
-  getPrintLabel,
-  handleResponse
+    t,
+    showTable,
+    setShowTable,
+    printTickets,
+    printTickets2,
+    printTickets3,
+    printTickets4,
+    getPrintLabel,
+    handleResponse,
 }) => {
-  const [loading, setLoading] = useState(false);
-  const [validationError, setValidationError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [validationError, setValidationError] = useState(false);
 
-  // const validation = Yup.object().shape({
-  //   ticket: Yup.string().required(t("loginScreen.enterYourEmail")),
-  //   tracking: Yup.string().required(t("registerScreen.enterYourFirstName")),
-  //   piece: Yup.string().required(t("registerScreen.enterYourLastName")),
-  // });
-  const formik = useFormik({
-    initialValues,
-    onSubmit: (values) => {
-      const callback = ({success, data})=>{
+    const formik = useFormik({
+        initialValues,
+        onSubmit: (values) => {
+            const callback = ({ success, data }) => {
+                if (success) {
+                    setLoading(false);
+                    handleResponse(data);
+                    setShowTable();
+                }
+            };
+            const val = Object.values(values);
+            if (!val.some((v) => v.trim() !== "")) {
+                setValidationError(true);
+                return;
+            }
+            setValidationError(false);
+            setLoading(true);
+            getPrintLabel({
+                requestId: values.request_id,
+                voucher: values.voucher,
+                delivery: values.delivery,
+                callback,
+            });
+        },
+    });
 
-        if(success){
-          setLoading(false);
-          handleResponse(data);
-          setShowTable();
-        }
-      }
-      const val = Object.values(values);
-      if (!val.some((v) => v.trim() !== "")) {
-        setValidationError(true);
-        return;
-      }
-      setValidationError(false);
-      setLoading(true);
-      getPrintLabel({
-        requestId: values.request_id,
-        voucher: values.voucher,
-        delivery: values.delivery,
-        callback
-      })
-     
-    },
-  });
-  
-  let headers = {
-    "Access-Control-Allow-Origin": "*",
-  };
+    let headers = {
+        "Access-Control-Allow-Origin": "*",
+    };
 
-  return (
-    <Form onSubmit={formik.handleSubmit}>
-      <Form.Row className="align-items-center mt-3">
-        <Col xs={6} md={3} className="mt-3">
-          <Form.Label>{t("printLabelTable.idRequest")}</Form.Label>
-          <Form.Control
-            name="request_id"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.request_id}
-          />
-        </Col>
-        <Col xs={6} md={3} className="mt-3">
-          <Form.Label>{t("printLabelScreen.tracking")}</Form.Label>
-          <Form.Control
-            name="voucher"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.voucher}
-          />
-        </Col>
-        <Col xs={6} md={3} className="mt-3">
-          <Form.Label>{t("printLabelScreen.piece")}</Form.Label>
-          <Form.Control
-            name="delivery"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.delivery}
-          />
-        </Col>
+    return (
+        <Form onSubmit={formik.handleSubmit}>
+            <Form.Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridRequestId">
+                    <Form.Label>{t("printLabelTable.idRequest")}</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="request_id"
+                        value={formik.values.request_id}
+                        onChange={formik.handleChange}
+                    />
+                </Form.Group>
 
-        <Col xs={6} md={3} className="mt-3">
-          <Form.Label style={{ color: "white" }}>Buscar</Form.Label>
-          <ActionButton disabled={loading} width="100" type="submit">
-            {loading ? (
-              <Spinner animation="border" variant="light" />
-            ) : (
-              t("printLabelScreen.search")
+                <Form.Group as={Col} controlId="formGridVoucher">
+                    <Form.Label>{t("printLabelScreen.tracking")}</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="voucher"
+                        value={formik.values.voucher}
+                        onChange={formik.handleChange}
+                    />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridDelivery">
+                    <Form.Label>{t("printLabelScreen.piece")}</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="delivery"
+                        value={formik.values.delivery}
+                        onChange={formik.handleChange}
+                    />
+                </Form.Group>
+            </Form.Row>
+
+            {validationError && (
+                <div className="alert alert-danger">
+                    <FontAwesomeIcon icon={faExclamationCircle} />{" "}
+                    {t("formPieces.validationError")}
+                </div>
             )}
-          </ActionButton>
-        </Col>
-        {validationError && (
-          <Col xs={12} className="mt-3">
-            <FontAwesomeIcon
-              className="text-danger mr-2"
-              icon={faExclamationCircle}
-            />
-            <span className="text-danger">
-              <strong>
-                Debe completar al menos un campo para poder realizar una
-                búsqueda.
-              </strong>
-            </span>
-          </Col>
-        )}
-        <Col xs={6} md={3}>
-          <Form.Label style={{ color: "white" }}>Imprimir Etiquetas</Form.Label>
-          {showTable && (
-            <ActionButton width="100" type="button" onClick={($event)=>printTickets($event)}>
-              {t("printLabelScreen.print")}
-            </ActionButton>
-          )}
-        </Col>
-      </Form.Row>
-    </Form>
-  );
+
+            <div className="d-flex align-items-center">
+                <ActionButton type="submit" className="mr-2" disabled={loading}>
+                    {loading ? (
+                        <Spinner animation="border" size="sm" />
+                    ) : (
+                        t("printLabelScreen.search")
+                    )}
+                </ActionButton>
+                {showTable && (
+                    <>
+                        <ActionButton
+                            type="button"
+                            className="mr-2"
+                            onClick={($event) => printTickets($event)}
+                        >
+                            Imprimir A4 Estandar
+                        </ActionButton>
+
+                        <ActionButton className="mr-2" onClick={printTickets2}>
+                            Imprimir 10x20
+                        </ActionButton>
+                        <ActionButton className="mr-2" onClick={printTickets3}>
+                            Imprimir 10x15
+                        </ActionButton>
+                        <ActionButton onClick={printTickets4}>
+                            Imprimir 10x10
+                        </ActionButton>
+                    </>
+                )}
+            </div>
+        </Form>
+    );
 };
-const mapStateToProps = ({ auth }) => ({ auth });
+
+const mapDispatchToProps = ({ auth, printLabels, user, tracking }) => ({
+    auth,
+    printLabels,
+    user,
+    tracking,
+});
 export default compose(
-  connect(mapStateToProps, { ...authActions }),
-  withTranslation()
+    connect(mapDispatchToProps, { ...authActions }),
+    withTranslation()
 )(FormPieces);
